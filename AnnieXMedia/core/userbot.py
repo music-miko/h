7855032e0e9cf92e3b827d91b045f7dccc531985
music-miko <voilet856@gmail.com>
2025-11-30 07:04:45 +0530

    Update userbot.py

--- AnnieXMedia/core/userbot.py
+++ AnnieXMedia/core/userbot.py
@@ -9,11 +9,9 @@ assistants = []
 assistantids = []
 
 GROUPS_TO_JOIN = [
-    "CertifiedDiscussion",
-    "CertifiedCoders",
-    "CertifiedCodes",
-    "CertifiedDevs",
-    "CertifiedNetwork",
+    "ArcChatz",
+    "ArcBotz",
+    "ArcUpdates",
 ]
 
 
@@ -79,7 +77,7 @@ class Userbot:
 
             try:
                 await client.send_message(
-                    config.LOGGER_ID, f"Annie's Assistant {index} Started"
+                    config.LOGGER_ID, f"Assistant {index} Started"
                 )
             except Exception:
                 LOGGER(__name__).error(
@@ -97,7 +95,7 @@ class Userbot:
             LOGGER(__name__).error(f"Failed to start Assistant {index}: {e}")
 
     async def start(self):
-        LOGGER(__name__).info("Starting Annie's Assistants...")
+        LOGGER(__name__).info("Starting Assistants...")
         await self.start_assistant(self.one, 1)
         await self.start_assistant(self.two, 2)
         await self.start_assistant(self.three, 3)
