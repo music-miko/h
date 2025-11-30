c0ffd050fc51ae7d89d99a4671f3bb82186e51ca
music-miko <voilet856@gmail.com>
2025-11-30 12:08:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -96,7 +96,7 @@ DURATION_LIMIT = time_to_seconds(f"{DURATION_LIMIT_MIN}:00")
 # ───── Bot Introduction Messages ───── #
 AYU = ["💞", "🦋", "🔍", "🧪", "⚡️", "🔥", "🎩", "🌈", "🍷", "🥂", "🥃", "🕊️", "🪄", "💌", "🧨"]
 WELCOME_TEXT = (
-    "<b>Hello {0},</b>\nI'm <b>{1}</b>, your music streaming bot for Telegram.\nI can play songs and videos in voice chats with smooth, high-quality playback from YouTube, Spotify, Resso, Apple Music and more.\n\n<b>Status:</b>\n• Uptime: <code>{2}</code>\n• Storage: <code>{3}</code>\n• CPU: <code>{4}</code>\n• RAM: <code>{5}</code>\n• Users: <code>{6}</code>\n• Chats: <code>{7}</code>\n\n<b>Powered By:</b> <a href='https://t.me/ArcBotz'>Arc Bots</a>"
+"<b>𝖧𝖾𝗒𝗒</b> {0}, 🌷\n\n๏ 𝖸𝗈𝗎’𝗋𝖾 𝗍𝖺𝗅𝗄𝗂𝗇𝗀 𝗍𝗈 {1}!\n\n➻ 𝖠 𝗉𝗋𝖾𝗆𝗂𝗎𝗆, 𝗉𝗈𝗐𝖾𝗋𝗉𝖺𝖼𝗄𝖾𝖽 𝖬𝗎𝗌𝗂𝖼 𝖡𝗈𝗍 𝗍𝗈 𝗅𝗂𝗀𝗁𝗍 𝗎𝗉 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉 𝗏𝗂𝖻𝖾𝗌 🎶\n───────────────────────────\n<b>๏ 𝖳𝗋𝗒 <code>/help</code> 𝗍𝗈 𝖽𝗂𝗌𝖼𝗈𝗏𝖾𝗋 𝖺𝗅𝗅 𝗍𝗁𝖾 𝖿𝗎𝗇 𝖼𝗈𝗆𝗆𝖺𝗇𝖽𝗌 𝖺𝗇𝖽 𝖿𝖾𝖺𝗍𝗎𝗋𝖾𝗌!</b>"
 )
 
 # ── Runtime structures ─────────────────────────────────────────────────────────
