0b664bab3a2926873964372cc9765c14223820a6
music-miko <voilet856@gmail.com>
2025-12-02 13:23:26 +0530

    Update queue.py

--- AnnieXMedia/utils/inline/queue.py
+++ AnnieXMedia/utils/inline/queue.py
@@ -65,12 +65,6 @@ def queue_back_markup(_, CPLAY):
 
 def aq_markup(_, chat_id):
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"stream_admin Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"stream_admin Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"stream_admin Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"stream_admin Stop|{chat_id}"),
-        ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
