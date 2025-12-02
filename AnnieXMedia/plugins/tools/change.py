15f347002b0109c0ea5e98f0f8251e40b1fc51d0
music-miko <voilet856@gmail.com>
2025-12-03 04:29:48 +0530

    Add video command to enable download first mode

---
+++ AnnieXMedia/plugins/tools/change.py
@@ -0,0 +1,8 @@
+from pyrogram import Client, filters
+from config import OWNER_ID
+from AnnieXMedia.utils.database import onoffdb
+
+@app.on_message(filters.command("video") & filters.user(OWNER_ID))
+async def enable_download_first(_, message):
+    await onoffdb.update_one({"on_off": 1}, {"$set": {"on_off": 1}}, upsert=True)
+    await message.reply("🔄 Video mode updated:\n📥 Download First → Stream fallback enabled!")
