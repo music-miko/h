dcc11f02e923dbf5006f118796c9ace485fb2619
music-miko <voilet856@gmail.com>
2025-12-30 15:02:25 +0530

    Update Spotify.py

--- AnnieXMedia/platforms/Spotify.py
+++ AnnieXMedia/platforms/Spotify.py
@@ -3,7 +3,7 @@ import re
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from youtubesearchpython import VideosSearch
+from youtubesearchpython.aio import VideosSearch
 
 import config
 
