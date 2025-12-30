b2be5b3fd75d1d5f2573594faacda4fd6a3953af
music-miko <voilet856@gmail.com>
2025-12-30 15:02:09 +0530

    Update Resso.py

--- AnnieXMedia/platforms/Resso.py
+++ AnnieXMedia/platforms/Resso.py
@@ -4,7 +4,7 @@ from typing import Union
 
 import aiohttp
 from bs4 import BeautifulSoup
-from youtubesearchpython import VideosSearch
+from youtubesearchpython.aio import VideosSearch
 
 
 class RessoAPI:
