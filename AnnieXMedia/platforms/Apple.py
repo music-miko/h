9c936e97b3bf091092d35454b170faf011601f20
music-miko <voilet856@gmail.com>
2026-01-05 13:01:25 +0530

    Update import statement for VideosSearch

--- AnnieXMedia/platforms/Apple.py
+++ AnnieXMedia/platforms/Apple.py
@@ -4,7 +4,7 @@ from typing import List, Union, Optional
 
 import aiohttp
 from bs4 import BeautifulSoup
-from youtubesearchpython.aio import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 
 class AppleAPI:
     def __init__(self):
