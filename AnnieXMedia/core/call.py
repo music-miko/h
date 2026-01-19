3b9c78477aa7924f9964e00b5007ff83bdef4516
music-miko <voilet856@gmail.com>
2026-01-19 09:54:42 +0530

    Update call.py

--- AnnieXMedia/core/call.py
+++ AnnieXMedia/core/call.py
@@ -347,7 +347,7 @@ class Call:
                         for track in random_choices:
                             buttons.append([
                                 InlineKeyboardButton(
-                                    text=f"▶️ {track['title'][:25]}...", 
+                                    text=f"{track['title'][:25]}...", 
                                     callback_data=f"suggestion|{track['vidid']}"
                                 )
                             ])
