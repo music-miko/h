93564025c59b1d17b1ab3a733e7771dcd8303353
music-miko <voilet856@gmail.com>
2026-02-02 01:28:11 +0530

    Update bot.py

--- AnnieXMedia/core/bot.py
+++ AnnieXMedia/core/bot.py
@@ -14,8 +14,8 @@ class MusicBotClient(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            workers=300,
-            max_concurrent_transmissions=100,
+            workers=500,
+            max_concurrent_transmissions=150,
         )
         LOGGER(__name__).info("Bot client initialized.")
 
